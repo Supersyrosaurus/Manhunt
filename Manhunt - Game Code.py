dd00f8a00647c90b5415bda304448abec21b0d8e
Supersyrosaurus <118223878+Supersyrosaurus@users.noreply.github.com>
2022-11-21 19:38:49 +0000

    Close window
    lines 9-13 keep a window open until a user clicks the cross button which then closes the whole window

--- Manhunt - Game Code.py
+++ Manhunt - Game Code.py
@@ -3,11 +3,11 @@ import pygame
 #Initialising the pygame module
 pygame.init()
 
-keepScreen = True
+#Creating a screen
+screen = pygame.display.set_mode((800, 600))
 
-while keepScreen == True:
-    #Creating a screen
-    screen = pygame.display.set_mode((800,600))
-    input = int(input('Keep screen?'))
-    if input == 1:
-        keepScreen = False
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
