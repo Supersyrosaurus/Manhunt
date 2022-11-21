e5875bdeeead15988518152e41bc9d8a068bfa70
Supersyrosaurus <118223878+Supersyrosaurus@users.noreply.github.com>
2022-11-21 19:06:21 +0000

    Simple loop to constantly display screen

--- Manhunt - Game Code.py
+++ Manhunt - Game Code.py
@@ -3,6 +3,11 @@ import pygame
 #Initialising the pygame module
 pygame.init()
 
-#Creating a screen
-screen = pygame.display.set_mode((800,600))
+keepScreen = True
 
+while keepScreen == True:
+    #Creating a screen
+    screen = pygame.display.set_mode((800,600))
+    input = int(input('Keep screen?'))
+    if input == 1:
+        keepScreen = False
