3d29e8076eaa38e91a36d1a6791568b01fbff346
Supersyrosaurus <118223878+Supersyrosaurus@users.noreply.github.com>
2022-11-22 18:54:12 +0000

    Thought that I needed the png in the files

--- Manhunt - Game Code.py
+++ Manhunt - Game Code.py
@@ -4,10 +4,24 @@ import pygame
 pygame.init()
 
 #Creating a screen
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1440,810))
 
+#Setting a title
+pygame.display.set_caption("Manhunt")
+
+#Setting the icon
+gameIcon = pygame.image.load('Manhunt.png')
+pygame.display.set_icon(gameIcon)
+
+#Loop for game screen
 running = True
 while running:
     for event in pygame.event.get():
+        #Checks through all of the events that are happening in the window
+        #If the event is the user pressing the quit button(pygame.QUIT) then the window is closed
         if event.type == pygame.QUIT:
             running = False
+    
+    #Changing background colour
+    screen.fill((75,75,75))
+    pygame.display.update()
