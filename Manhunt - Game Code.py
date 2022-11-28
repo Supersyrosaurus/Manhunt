8bb5ffc63efe76f632bae62590d5763bdb417903
Supersyrosaurus <118223878+Supersyrosaurus@users.noreply.github.com>
2022-11-28 11:41:30 +0000

    Started button class
    Created a button class with which is passed the image, x and y coordinates and also a scale. It is then transformed and also has a method that can be called to draw it on the screen

--- Manhunt - Game Code.py
+++ Manhunt - Game Code.py
@@ -15,6 +15,25 @@ pygame.display.set_caption("Manhunt")
 gameIcon = pygame.image.load('Manhunt.png')
 pygame.display.set_icon(gameIcon)
 
+print(str(gameIcon.get_rect()))
+print(str(gameIcon.get_width()))
+print(str(gameIcon.get_height()))
+
+class Button():
+    def __init__(self, img, x, y, scale):
+        self.img = pygame.image.load(img).convert_alpha()
+        width = self.img.get_width()
+        height = self.img.get_height()
+        self.transformedImg = pygame.transform.scale(self.img, (int(width * scale), int(height * scale)))
+        self.rect = self.transformedImg.get_rect()
+        self.topLeft = (x, y)
+
+    def draw(self):
+
+        screen.blit(self.transformedImg, self.topLeft)
+    
+    '''def clickCheck(self, )'''
+
 #Procedure to display image
 def displayImg(imgName, x, y):
     #Loads the image
@@ -29,7 +48,7 @@ def displayText(textName, x, y, size, colour = (0,0,0), font = None):
     text = textFont.render(textName, True,  colour)
     screen.blit(text, (x, y))
 
-
+startButton = Button('rectangleStart.png', 250, 150, 0.2)
 
 #Loop for game screen
 running = True
@@ -45,5 +64,5 @@ while running:
     screen.fill((75,75,75))
     displayText('Manhunt', 230, 80, 160)
     displayImg('Manhunt.png', 32, 32)
-    displayImg('rectangleStart.png', 350, 160)
+    startButton.draw()
     pygame.display.update()
