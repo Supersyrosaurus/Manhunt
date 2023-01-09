69fabe8cdee0606e82f38bb76619c4166bfec28b
Supersyrosaurus <118223878+Supersyrosaurus@users.noreply.github.com>
2023-01-09 14:01:12 +0000

    started player

--- player.py
+++ player.py
@@ -2,3 +2,7 @@ import pygame
 pygame.init()
 
 class Player():
+    def __init__(self, x, y):
+        coords = (x, y)
+        self.playerImg = pygame.image.load('whiteCircle.png').convert_alpha()
+        self.playerHitbox = self.playerImg.get_rect()
