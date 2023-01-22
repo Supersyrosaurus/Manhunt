c503b6848b75c6ce3323f6ece27347fbcf1f148b
Supersyrosaurus <118223878+Supersyrosaurus@users.noreply.github.com>
2023-01-22 21:15:18 +0000

    started using sprites in player class

--- player.py
+++ player.py
@@ -1,8 +1,13 @@
 import pygame
 pygame.init()
 
-class Player():
-    def __init__(self, x, y):
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, colour):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(colour)
+        self.rect = self.image.get_rect()
+        
         coords = (x, y)
         self.playerImg = pygame.image.load('whiteCircle.png').convert_alpha()
         self.playerHitbox = self.playerImg.get_rect()
